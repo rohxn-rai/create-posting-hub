5a421098e6b02eab5d6cbc2c94d66684c680066f
rohxn-rai <rohan.rai932@outlook.com>
2025-02-20 06:26:31 -0500

    modified from submit component - added a pending status fallback test

--- components/form-submit.js
+++ components/form-submit.js
@@ -5,6 +5,10 @@ import { useFormStatus } from "react-dom";
 export default function FormSubmit() {
   const status = useFormStatus();
 
+  if (status.pending) {
+    return <p>Creating Post...</p>;
+  }
+
   return (
     <>
       <button type="reset">Reset</button>
