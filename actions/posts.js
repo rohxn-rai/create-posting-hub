61d87755cb063d91850e24193b55b4261c3ffe9b
rohxn-rai <rohan.rai932@outlook.com>
2025-02-24 06:13:23 -0500

    modified post.js - added a function to handle likes on post

--- actions/posts.js
+++ actions/posts.js
@@ -2,7 +2,7 @@
 
 import { redirect } from "next/navigation";
 
-import { storePost } from "@/lib/posts";
+import { storePost, updatePostLikeStatus } from "@/lib/posts";
 import { uploadImage } from "@/lib/cloudinary";
 
 export async function createPost(prevState, formData) {
@@ -44,4 +44,8 @@ export async function createPost(prevState, formData) {
   });
 
   redirect("/feed");
+}
+
+export async function togglePostLikeStatus(postId) {
+  updatePostLikeStatus(postId, 2);
 }
