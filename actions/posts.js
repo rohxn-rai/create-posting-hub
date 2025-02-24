75aa397778bf5fafad7e3d0d6f0b23cc639273f3
rohxn-rai <rohan.rai932@outlook.com>
2025-02-24 04:44:15 -0500

    modified posts.js - added actual upload data content to cloudinary server

--- actions/posts.js
+++ actions/posts.js
@@ -3,6 +3,7 @@
 import { redirect } from "next/navigation";
 
 import { storePost } from "@/lib/posts";
+import { uploadImage } from "@/lib/cloudinary";
 
 export async function createPost(prevState, formData) {
   const title = formData.get("title");
@@ -26,9 +27,17 @@ export async function createPost(prevState, formData) {
   if (errors.length > 0) {
     return { errors };
   }
+  let imageUrl;
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (error) {
+    throw new Error(
+      "Image upload failed, post was not created. Please try again later."
+    );
+  }
 
   await storePost({
-    imageUrl: "",
+    imageUrl: imageUrl,
     title,
     content,
     userId: 1,
